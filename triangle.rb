d6e4e6e0aa8f5fd6450edfb08edd828c4789edcc
Abhishek Jain <ajj.2396@gmail.com>
2019-02-12 19:10:53 +0530

    Completed triangles_project_2

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,18 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if a<=0 || b<=0 || c<=0
+    raise TriangleError
+  end
+
+  length_array = [a,b,c]
+  length_array.sort!
+  
+  puts length_array
+  if length_array[0]+length_array[1]<=length_array[2]
+    raise TriangleError
+  end
+
   if a==b && b==c
     return :equilateral
   elsif ((a==b)&&(a!=c)&&(b!=c))||((a!=b)&&(b==c)&&(a!=c))||((a==c)&&(b!=c)&&(a!=b))
